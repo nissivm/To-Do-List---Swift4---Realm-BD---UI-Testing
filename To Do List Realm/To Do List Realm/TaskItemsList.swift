80b3e2b0b01dedd9555b4f8b095570e79a98f7d3
nissivm <nissivm@yahoo.com.br>
2018-04-23 09:52:28 +0100

    - Removed files ViewController.swift & Main.storyboard - Created new files TasksList.swift/.xib & TaskItems.swift/.xib

---
+++ To Do List Realm/To Do List Realm/TaskItemsList.swift
@@ -0,0 +1,35 @@
+//
+//  TaskItemsList.swift
+//  To Do List Realm
+//
+//  Created by Altran Portugal Fundão Nissi on 23/04/2018.
+//  Copyright © 2018 App Magic. All rights reserved.
+//
+
+import UIKit
+
+class TaskItemsList: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
